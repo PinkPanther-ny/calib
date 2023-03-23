574a0a8f8ffe890e68bf13f5f8c0b16f248b038d
PinkPanther-ny <alvincny529@gmail.com>
2023-03-23 22:26:02 +0800

    Use NumPy for vectorized operations and eliminate the double for-loop

--- core/pixel2world.py
+++ core/pixel2world.py
@@ -24,13 +24,13 @@ class CoordinateConverter:
         self.camera_matrix = camera_matrix
         self.dist_coeffs = dist_coeffs
         self.camera_position = Vector3(*camera_position)
-        self.towards_direction = Vector3(*towards_direction)
+        self.towards_direction = Vector3(*towards_direction).normalized()
         self.ground_plane = Plane(Vector3(0, 0, 0), Vector3(0, 1, 0))
         
         self.pixel_is_distort = pixel_is_distort
 
         t0 = time.time()
-        self._create_world_coordinate_map()
+        self._create_world_coordinate_map_fast()
         print(f"Coordinate map ({frame_width}, {frame_height}) computed in {time.time() - t0} secs.")
 
     def _calculate_fov(self) -> Tuple[float, float]:
@@ -68,12 +68,12 @@ class CoordinateConverter:
         y = (1 - 2 * (pixel[1] + 0.5) / self.frame_height) * np.tan(fov_y / 2 * np.pi / 180)
         pixel_world_direction = Vector3(x, y, 1).normalized()
 
-        rotation_axis = self.towards_direction.normalized().cross(Vector3(0, 0, 1))
-        rotation_angle = np.arccos(self.towards_direction.normalized().dot(Vector3(0, 0, 1)))
-        rotation_matrix = cv2.Rodrigues(np.array([rotation_axis.x, rotation_axis.y, rotation_axis.z]) * rotation_angle)[0]
-        rotated_direction = np.dot(rotation_matrix, np.array([pixel_world_direction.x, pixel_world_direction.y, pixel_world_direction.z]))
+        rotation_axis = self.towards_direction.cross(Vector3(0, 0, 1))
+        rotation_angle = np.arccos(self.towards_direction.dot(Vector3(0, 0, 1)))
+        rotation_matrix = cv2.Rodrigues(rotation_axis.to_array() * rotation_angle)[0]
+        rotated_direction = np.dot(rotation_matrix, pixel_world_direction.to_array())
 
-        rotated_pixel_world_direction = Vector3(rotated_direction[0], rotated_direction[1], rotated_direction[2]).normalized()
+        rotated_pixel_world_direction = Vector3(*rotated_direction).normalized()
 
         ray = Ray(self.camera_position, rotated_pixel_world_direction)
         intersection = self.ground_plane.intersect(ray)
@@ -89,3 +89,35 @@ class CoordinateConverter:
             for j in range(self.frame_width):
                 world_coord = self._pixel_to_world_coordinate((j, i))
                 self.world_coordinate_map[i, j] = [world_coord.x, world_coord.y, world_coord.z]
+
+    def _create_world_coordinate_map_fast(self):
+        self.world_coordinate_map = np.empty((self.frame_height, self.frame_width, 3), dtype=np.float32)
+        
+        pixel_x, pixel_y = np.meshgrid(np.arange(self.frame_width, dtype=np.float32), np.arange(self.frame_height, dtype=np.float32))
+        pixel_coords = np.vstack((pixel_x.flatten(), pixel_y.flatten())).T
+        
+        if self.pixel_is_distort:
+            undistorted_pixel_coords = cv2.undistortPoints(pixel_coords.reshape(-1, 1, 2), self.camera_matrix, self.dist_coeffs, P=self.camera_matrix)
+            pixel_coords = undistorted_pixel_coords.reshape(-1, 2)
+
+        fov_x, fov_y = self._calculate_fov()
+        aspect_ratio = self.frame_width / self.frame_height
+        
+        x = (2 * (pixel_coords[:, 0] + 0.5) / self.frame_width - 1) * np.tan(fov_x / 2 * np.pi / 180) * aspect_ratio
+        y = (1 - 2 * (pixel_coords[:, 1] + 0.5) / self.frame_height) * np.tan(fov_y / 2 * np.pi / 180)
+        pixel_world_direction = np.vstack((x, y, np.ones_like(x)))
+        pixel_world_direction = (pixel_world_direction / np.linalg.norm(pixel_world_direction.T, axis=1)).T
+
+        rotation_axis = self.towards_direction.cross(Vector3(0, 0, 1))
+        rotation_angle = np.arccos(self.towards_direction.dot(Vector3(0, 0, 1)))
+        rotation_matrix = cv2.Rodrigues(rotation_axis.to_array() * rotation_angle)[0]
+        rotated_direction = np.dot(pixel_world_direction, rotation_matrix.T)
+
+        rotated_pixel_world_direction = (rotated_direction.T / np.linalg.norm(rotated_direction, axis=1)).T
+
+        intersection = self.ground_plane.intersect_many(
+            ray_origin=self.camera_position.to_array(), 
+            ray_directions=np.array(rotated_pixel_world_direction)
+        )
+
+        self.world_coordinate_map = intersection.reshape(self.frame_height, self.frame_width, 3)
